fe2b44163b40fff77be389722e551051cef0cf61
dlccyes <derricken968@gmail.com>
2021-07-18 22:39:32 +0800

    init

---
+++ app.js
@@ -0,0 +1,235 @@
+function login(){
+    var url='https://accounts.spotify.com/authorize'
+    url += "?client_id=" + client_id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-playback-state playlist-read-private"
+    location.href = url;
+}
+// })
+
+function get_token(){
+    var newurl = String(window.location);
+    var code = newurl.slice(newurl.search(/=/)+1,);
+    var tolkien = null;
+    $.ajax({
+        method: "POST",
+        url: "https://accounts.spotify.com/api/token",
+        data: {
+          "grant_type":    "authorization_code",
+          "code":          code,
+          "redirect_uri":  redirect_uri,
+          "client_id":     client_id,
+          "client_secret": client_secret,
+        },
+        async: false,
+        success: function(result){
+            tolkien = result['access_token'];
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log('error ' + textStatus);
+            console.log(jqXHR);
+        },
+        timeout: 5000
+    });
+    return tolkien; 
+}
+
+function spott_get(url, token, callback, async=true){
+    if(token){
+        $.ajax({
+            async: async,
+            url : url,
+            dataType : 'json',
+            type : 'GET',
+            // async: false;
+            headers : {
+                // 'Acccept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer '+ token,
+            },
+            success: function(xhr) {
+                if(callback){
+                    callback(xhr);
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log('error ' + textStatus);
+                console.log(jqXHR);
+            },
+            timeout: 5000
+        });
+    }else{
+        alert('please login first');
+    }
+}
+
+function spott_get_sync(url, token, callback){ //sync version
+    spott_get(url, token, callback, async=false);
+}
+
+function get_all_playlists(){
+    if(token){
+        var continuue = true;
+        // if(playlists.length != 0){ //alreadt executed
+        //     continuue = false;
+        // }
+        var next_url = 'https://api.spotify.com/v1/me/playlists?limit=50';
+        while(continuue){
+            spott_get_sync(next_url, token, function(xhr){
+                // console.log(xhr['items']);
+                playlists = playlists.concat(xhr['items']);
+                // for(var i in xhr['items']){
+                //     playlists.push(xhr['items'][i]);
+                // }
+                if(xhr['next']){
+                    next_url = xhr['next'];
+                }else{
+                    continuue = false;
+                }
+            });
+        }
+    }else{
+        console.log('no token');
+    }
+
+}
+
+function ArtistDistribution(tracks){
+    var artists = {};
+    for(var item of tracks){
+        // console.log(item);
+        for(var artist of item['track']['artists']){
+            if(!artists[artist['name']]){
+                artists[artist['name']] = 0;
+            }
+            artists[artist['name']] += 1;
+        }
+    }
+    // console.log(artists);
+    return sortDict(artists);
+}
+
+function sortDict(ogDict) {
+    var keys = keys = Object.keys(ogDict);
+    sortedKey = keys.sort(function(a,b){return ogDict[b]-ogDict[a]});
+    sortedArr = [];
+    // console.log(sortedKey);
+    for(var key of sortedKey){
+        sortedArr.push([key, ogDict[key]]);
+    }
+    // console.log(sortedArr);
+    return sortedArr;
+}
+
+function get_playlist_details(){
+    if(token){
+        get_all_playlists();
+        name = $('#playlist_input').val();
+        var playlist_id = '';
+        var playlist_name = '';
+        if($("#playlistExactMatch").prop("checked")==true){ //need exact match
+            for(var i=0; i<playlists.length; i++) {
+                if(playlists[i]['name'] == name){ //ignore case and search
+                    playlist_id = playlists[i]['id'];
+                    playlist_name = playlists[i]['name'];
+                }
+            }        
+        }else{
+            for(var i=0; i<playlists.length; i++){ //no need exact match
+                if(playlists[i]['name'].toLowerCase().indexOf(name.toLowerCase()) != -1){ //ignore case and search
+                    playlist_id = playlists[i]['id'];
+                    playlist_name = playlists[i]['name'];
+                }
+            }
+        }
+        var current_playlist;
+        if(playlist_id != ''){
+            spott_get_sync('https://api.spotify.com/v1/playlists/'+playlist_id, token, function(xhr){
+                current_playlist = xhr;
+            });
+            var continuue = true
+            var next_url = 'https://api.spotify.com/v1/playlists/'+playlist_id+'/tracks?limit=100';
+            var temp = [];
+            i=0;
+            while(continuue){
+                spott_get_sync(next_url, token, function(xhr){
+                    temp = temp.concat(xhr['items']);
+                    if(xhr['next']){
+                        next_url = xhr['next'];
+                    }else{
+                        continuue = false;
+                    }
+                });
+                i++
+                if(i>=5){
+                    break;
+                }
+            }
+            current_playlist['tracks']['items'] = temp; //replace
+            sortedArtistArr = ArtistDistribution(current_playlist['tracks']['items']);
+            sortedArtistArr.splice(0,0,['Artist','Number']);
+            image_url = current_playlist['images'][0]['url'];
+            playlistDivhtml = '<div id="PlaylistMeta">\
+                                    <h1>'+playlist_name+'</h1>\
+                                    <img src="'+image_url+'" width="200px" style="height: 350px;border-radius: 20px;width: auto;">\
+                                </div>\
+                                <div id="ArtistDiv">\
+                                    <div id="ArtistGraph" style="float: left;">\
+                                        <h2 style="margin: 0;">Artists pie chart of '+playlist_name+'</h2>\
+                                        <div id="ArtistPiechart" style="width: 900px; height: 500px; margin-top: -45px;""></div>\
+                                    </div>\
+                                    <div id="ArtistListDiv">\
+                                        <h2>top 10 artists</h2>\
+                                    </div>\
+                                </div>';
+            $('#currentPlaylistDiv').html(playlistDivhtml);
+
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(function(){
+                var data = google.visualization.arrayToDataTable(sortedArtistArr);
+                var options = {
+                  // title: 'Artist pie chart of '+playlist_name,
+                    width: 800,
+                    pieHole: 0.2,
+                    backgroundColor: {
+                        fill: 'transparent',
+                        // stroke: '#000',
+                    },
+                    legend: {
+                        textStyle:{
+                            color: '#fff'
+                        }
+                    },
+                    // pieSliceBorderColor: 'transparent',
+                    sliceVisibilityThreshold: .0041,
+                };
+                var chart = new google.visualization.PieChart(document.getElementById('ArtistPiechart'));
+                chart.draw(data, options);    
+            });
+
+            // $('#currentPlaylistDiv').append('<div id="ArtistListDiv"></div>');
+            ArtistListhtml = '<table style=""><th>'+sortedArtistArr[0][0]+'</th>\
+                                <th>'+sortedArtistArr[0][1]+'</th>';
+            for(var i=1; i<=10; i++){
+                if(!sortedArtistArr[i]){ //is less than 10 artists
+                    break;
+                }
+                ArtistListhtml += '<tr><td>'+sortedArtistArr[i][0]+'</td>\
+                                    <td>'+sortedArtistArr[i][1]+'</td></tr>'
+            }
+            ArtistListhtml += '</table>'
+            $('#ArtistListDiv').append(ArtistListhtml);
+
+        }else{
+            alert('no result');
+            console.log('failure');
+        }
+        // playlist_id = $('#playlist_input').val();
+        // // `329i010xBb8kqife9WYVu7`
+
+    }else{
+        console.log('no token');
+    }
+}
---
+++ index.html
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel='stylesheet' href="style.css" type='text/css' media='all' /> 
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="config.js"></script>
+    <script src="app.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+</head>
+
+<body>
+
+<button id='login'>login to spotify</button>
+<button id='current_playback'>show current playback</button>
+<br>
+<!-- <button id='show_all_playlists'>show all playlists</button> -->
+<br>
+<div id='currentPlaybackDiv'></div>
+<br>
+<p>search playlist</p>
+<input type="text" id="playlist_input"></input>
+<input type="checkbox" id="playlistExactMatch">exact match</input>
+<button id='get_playlist'>show playlist detail</button>
+<br>
+<div id='currentPlaylistDiv'></div>
+
+
+<script>
+console.disableYellowBox = true;
+
+var scopes = 'user-read-private user-read-email';
+
+var url = String(window.location);
+if(url.search(/=/)!=-1){
+    var code = url.slice(url.search(/=/)+1,);
+}
+if(code){
+    token = get_token();
+}
+var playlists = [];
+
+$(document).ready(function(){
+    $('#login').click(function(){
+        login();
+    });
+
+    $('#show_all_playlists').click(function(){
+        if(playlists.length == 0){ //alreadt executed
+            get_all_playlists();
+        }
+    });
+
+    $('#current_playback').click(function(){
+        play = spott_get('https://api.spotify.com/v1/me/player', token, function(xhr){
+            console.log(xhr['item']['name'],'by',xhr['item']['artists'][0]['name']);
+            currentPlaybackhtml = xhr['item']['name']+' by '+xhr['item']['artists'][0]['name'];
+            $('#currentPlaybackDiv').html(currentPlaybackhtml);
+      });
+    });
+
+    $('#playlist_input').keypress(function(e){
+        if(e.which == 13){
+            get_playlist_details();
+        }
+    })
+
+    $('#get_playlist').click(function(){ //get full playlist details including all tacks
+        get_playlist_details();
+    });
+    
+
+
+});
+
+</script>
+</body>
+</html>
